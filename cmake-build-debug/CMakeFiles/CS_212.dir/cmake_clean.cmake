97e0dd972ffb9318c145522c14a386f24df99465
alptheexplorer <alpozen@gmail.com>
2021-02-23 15:43:01 +0100

    Task 1

---
+++ cmake-build-debug/CMakeFiles/CS_212.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS_212.dir/weekOne/age.c.obj"
+  "CS_212.exe"
+  "CS_212.exe.manifest"
+  "CS_212.pdb"
+  "libCS_212.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS_212.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
